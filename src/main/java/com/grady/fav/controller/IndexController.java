0e984002cefa3ca4a3a1bef8a0cbf59fd0181635
Grady <yhwei876@gmail.com>
2018-08-12 19:53:14 +0800

    添加注册登录页面。

--- src/main/java/com/grady/fav/controller/IndexController.java
+++ src/main/java/com/grady/fav/controller/IndexController.java
@@ -39,9 +39,28 @@ public class IndexController extends BaseController {
         return "index";
     }
 
+    /**
+     * Home 页面
+     */
     @GetMapping("/")
     public String home(Model model) {
         return "home";
+    }
+
+    /**
+     * Register页面
+     */
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    /**
+     * Login页面
+     */
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
 
